594f6a03775e615e0a1084ae9ce2b06ff58cdd36
Charles Billette <charles.billette@intel.com>
2017-03-28 13:15:53 -0400

    Carthage enable

---
+++ KKDomainFramework/KKDomainFramework.h
@@ -0,0 +1,19 @@
+//
+//  KKDomainFramework.h
+//  KKDomainFramework
+//
+//  Created by Charles Billette on 2017-03-28.
+//  Copyright © 2017 geeklu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KKDomainFramework.
+FOUNDATION_EXPORT double KKDomainFrameworkVersionNumber;
+
+//! Project version string for KKDomainFramework.
+FOUNDATION_EXPORT const unsigned char KKDomainFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKDomainFramework/PublicHeader.h>
+
+
